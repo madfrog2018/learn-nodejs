be97760d97420db11e5c9ce3b020e2a923201aee
huyang <huyang1532@gmail.com>
2014-04-18 17:30:46 +0800

    add the test array demo
    Signed-off-by: huyang <huyang1532@gmail.com>

---
+++ static/app/testArray.js
@@ -0,0 +1,9 @@
+define(function(requrie, exports){
+
+	var data = [];
+	for (var i = 0; i < 10; i++) {
+		
+		data.push(i);
+	}
+	exports.data = data;
+});
--- static/main/main.js
+++ static/main/main.js
@@ -1,6 +1,19 @@
 define(function(require){
 
 	var hello = require("app/hello");
-	alert(hello.increment(10));
+	console.log(hello.increment(10));
+	var result = [];
+	var result2 = [];
+	var testData = require("app/testArray");
+	for (var i = 0; i < testData.data.length; i++) {
+		resultData = testData.data[i++] += 2;
+		result.push(resultData);
+		result2Data = testData.data[i++] = testData.data[i++] + 1;
+		result2.push(result2Data);
+	};
+	
+	console.log(result);
+	console.log("---------");
+	console.log(result2);
 
 });
